7309c31c39360af687ffaf69769c317a13dee8ef
Joel Marcey <joelm@fb.com>
2017-01-13 16:06:08 -0800

    Add a simple standalone Java sample application

---
+++ hello-buck-java/README.md
@@ -0,0 +1,9 @@
+# Hello Buck - java
+
+A simple demo on how to setup Buck to build and execute a Java application.
+
+## Running
+
+```
+buck run hello_buck_java
+```
---
+++ hello-buck-java/com/facebook/buck/demo/HelloString.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.buck.demo;
+
+public class HelloString {
+  public static String getHelloString() {
+    return "Buck\n";
+  }
+}
